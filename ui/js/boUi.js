d53784dbb83baa8ccd6ee53f6c2c30e0952e24e9
wangjiaming <519677263@qq.com>
2017-06-20 16:45:43 +0800

    完善datagrid

--- ui/js/boUi.js
+++ ui/js/boUi.js
@@ -72,7 +72,7 @@
         }
     }
 
-    var defaultDataGridParams='',namespace,tmpBoDataGridData={data:''};
+    var defaultDataGridParams={},tmpBoDataGridData={};
     $.fn.boDataGrid=function(options){
         if ($.fn.boDataGrid.methods[options]) {
             return $.fn.boDataGrid.methods[ options ].apply( this, Array.prototype.slice.call( arguments, 1 ));
@@ -85,24 +85,21 @@
 
     $.fn.boDataGrid.methods = {
         getParams:function(options){
-            if(defaultDataGridParams==''){
-                defaultDataGridParams = {width:'100%',height:'100%',scene:'primary',pagesList:[10,30,50,100],rows:10,page:1,total:''};
-                defaultDataGridParams = $.extend({}, defaultDataGridParams, options);
+            if(defaultDataGridParams[this[0].id]==undefined){
+                defaultDataGridParams[this[0].id] = $.extend({},{namespace:this[0].id,width:'100%',height:'100%',scene:'primary',pagesList:[10,30,50,100],rows:10,page:1,total:''}, options);
             }
-            return defaultDataGridParams;
-
+            return defaultDataGridParams[this[0].id];
         },
         init:function (options) {
             var _this = this ;
-            namespace=_this[0].id;
             var _options=_this.boDataGrid('getParams',options);
-            $(_this).attr('style','width:100%;height:100%;overflow:auto');
+            $(_this).attr('style','width:100%;height:100%');
             $(_this).attr('class','panel panel-primary');
-            $(_this).append('<div id="boDataGridLoading-'+namespace+'" class="modal fade bs-example-modal-sm" tabindex="-1"'+
+            $(_this).append('<div id="boDataGridLoading-'+_options.namespace+'" class="modal fade bs-example-modal-sm" tabindex="-1"'+
                 'role="dialog" aria-labelledby="mySmallModalLabel">'+
                 '<div class="modal-dialog modal-sm" role="document"><div class="modal-content">'+
                 '<div style="text-align: center"><img src="../ui/img/boLoading.gif" style="width:30px">正在加载，请稍等...<div></div></div></div>');
-            $('#boDataGridLoading-'+namespace).modal({backdrop: 'static', keyboard: false});
+            $('#boDataGridLoading-'+_options.namespace).modal({backdrop: 'static', keyboard: false});
             if(_options.title){$(_this).append('<div class="panel-heading">'+_options.title+'</div>');}
             if(_options.toolbar!=undefined&&_options.toolbar.length>0){
                 var $toolbar=$('<div class="panel-body"></div>');
@@ -137,30 +134,49 @@
                 $toolbar.append($toolbarButton);
                 $(_this).append($toolbar);
             }
-            var $table=$('<table id="dgTable-'+namespace+'" class="table table-bordered table-hover"><tbody></tbody></table>'),$tableTitle;
+
+            var $table=$('<table id="dgTable-'+_options.namespace+'" class="table table-bordered table-hover"><tbody></tbody></table>'),$tableTitle;
             if(_options.tableTitle&&_options.tableTitle!=''){
                 $tableTitle=$(_options.tableTitle);
                 $tableTitle.addClass("success");
             }else{
-                $tr=$('<tr id="titleTr-'+namespace+'" class="success"></tr>');
+                $tr=$('<tr id="titleTr-'+_options.namespace+'" class="success"></tr>');
+                if(_options.checkbox){
+                    var $thCheck=$('<th width="10px"></th>');
+                    var $checkBox=$('<input id="allCheckBox-'+_options.namespace+'" type="checkbox"/>');
+                    $thCheck.append($checkBox);
+                    $tr.append($thCheck);
+                    $checkBox.change(function() { 
+                       $('#allCheckBox-'+_options.namespace)[0].checked
+                       $('#tableData-'+_options.namespace+' input[type="checkbox"]').each(function(){
+                             console.info(this)
+                       });
+
+                    });
+                }
                 for(var i=0;i<_options.columns[0].length;i++){
                     var thWidth='100px';
                     if(_options.columns[0][i].width&&_options.columns[0][i].width!=''){
                         thWidth=_options.columns[0][i].width;
                     }
                     var $th=$('<th width="'+thWidth+'" title="'+_options.columns[0][i].title+'"><div style="width:'+thWidth+';overflow:hidden">'+_options.columns[0][i].title+'</div></th>')
+                    if(_options.columns[0][i].hidden){
+                        $th.hide();
+                    }
                     $tr.append($th);
                 }
                 $tableTitle=$tr;
             }
             $table.append($tableTitle);
-            $dataDiv=$('<tbody id="tableData-'+namespace+'"></tbody>');
+            $dataDiv=$('<tbody id="tableData-'+_options.namespace+'"></tbody>');
             $table.append($dataDiv);
+            var $tableDiv=$('<div style="overflow:auto"></div>');
+            $tableDiv.append($table);
+            $(_this).append($tableDiv);
             $buttonFooter=$('<div class="panel-footer">'+
-                            '<div style="float:right" class="bg-danger" id="pageContent"></div>' +
+                            '<div style="float:right" class="bg-danger" id="pageContent-'+_options.namespace+'"></div>' +
                             '</div>');
-            $(_this).append($table);
-            var pagesListHtml='<div id="fyList-'+namespace+'" class="btn-group dropup" role="group">'+
+            var pagesListHtml='<div id="fyList-'+_options.namespace+'" class="btn-group dropup" role="group">'+
                 '<button id="_nowPageBtn" class="btn btn-default btn-sm dropdown-toggle" type="button" data-toggle="dropdown" '+
                 'aria-haspopup="true" aria-expanded="false">';
 
@@ -174,70 +190,72 @@
                 }
             }
             $fyButtons=$('<div class="btn-group-sm" role="group" aria-label="fy">'+
-                '<button type="button" class="btn btn-default" id="theTopPage"><span class="glyphicon glyphicon-step-backward" aria-hidden="true"></button>'+
-                '<button type="button" class="btn btn-default" id="topPage"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></button>'+
-                '<button type="button" class="btn btn-default" id="nowPage">1</button>'+
-                '<button type="button" class="btn btn-default" id="lastPage"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></button>'+
-                '<button type="button" class="btn btn-default" id="theLastPage"><span class="glyphicon glyphicon-step-forward" aria-hidden="true"></button>'+
+                '<button type="button" class="btn btn-default" id="theTopPage-'+_options.namespace+'"><span class="glyphicon glyphicon-step-backward" aria-hidden="true"></button>'+
+                '<button type="button" class="btn btn-default" id="topPage-'+_options.namespace+'"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></button>'+
+                '<button type="button" class="btn btn-default" id="nowPage-'+_options.namespace+'">1</button>'+
+                '<button type="button" class="btn btn-default" id="lastPage-'+_options.namespace+'"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></button>'+
+                '<button type="button" class="btn btn-default" id="theLastPage-'+_options.namespace+'"><span class="glyphicon glyphicon-step-forward" aria-hidden="true"></button>'+
                 pagesListHtml+
                 '</div>');
             $buttonFooter.append($fyButtons);
             $(_this).append($buttonFooter);
-            _this.boDataGrid('queryData');
-
+            if(typeof _options.onBeforeLoad== "function"){
+                if(_options.onBeforeLoad()!=false){
+                    _this.boDataGrid('queryData');
+                }
+            }else{
+                _this.boDataGrid('queryData');
+            }
             $('li').on('click',function(){
                 defaultDataGridParams.rows = $(this)[0].textContent;
                 $('#_nowPageBtn').html($(this)[0].textContent+'<span class="caret"></span>');
             });
 
-            $('#theTopPage,#topPage,#lastPage,#theLastPage').on('click',function(){
-                if($(this).attr('id')=='theTopPage'){
+            $('#theTopPage-'+_options.namespace+',#topPage-'+_options.namespace+',#lastPage-'+_options.namespace+',#theLastPage-'+_options.namespace+'').on('click',function(){
+                if($(this).attr('id')=='theTopPage-'+_options.namespace){
                     defaultDataGridParams.page='1';
-                }else if($(this).attr('id')=='topPage'){
+                }else if($(this).attr('id')=='topPage-'+_options.namespace){
                     if(defaultDataGridParams.page!='1'){
                         defaultDataGridParams.page=defaultDataGridParams.page-1;
                     }
-                }else if($(this).attr('id')=='lastPage'){
+                }else if($(this).attr('id')=='lastPage-'+_options.namespace){
                     var matchPage= Math.ceil(defaultDataGridParams.total/defaultDataGridParams.rows);
                     if(defaultDataGridParams.page<matchPage){
                         defaultDataGridParams.page=eval(defaultDataGridParams.page+1);
                     }
-                }else if($(this).attr('id')=='theLastPage'){
+                }else if($(this).attr('id')=='theLastPage-'+_options.namespace){
                     var matchPage= Math.ceil(defaultDataGridParams.total/defaultDataGridParams.rows);
                     defaultDataGridParams.page=matchPage;
                 }
-                $('#nowPage').html(defaultDataGridParams.page);
-                if(typeof _options.onBeforeLoad== "function"){
-                    if(_options.onBeforeLoad()!=false){
-                        _this.boDataGrid('queryData');
-                    }
-                }else{
-                    _this.boDataGrid('queryData');
-                }
+                $('#nowPage-'+_options.namespace).html(defaultDataGridParams.page);
+                _this.boDataGrid('queryData');
             });
         },
         queryData:function(){
             var _this = this;
-            $('#boDataGridLoading-'+namespace).modal('show');
             var _options =_this.boDataGrid('getParams');
-            var $tableData = $('#tableData-'+namespace);
-            $('#tableData-'+namespace+' tr').remove();
+            $('#boDataGridLoading-'+_options.namespace).modal('show');
+            var $tableData = $('#tableData-'+_options.namespace);
+            $('#tableData-'+_options.namespace+' tr').remove();
+            var $dataDiv=$('#tableData-'+_options.namespace)
             var params='rows='+_options.rows+'&page='+_options.page;
             if(_options.formId&&_options.formId!=''&&$('#'+_options.formId).serialize().length>'0'){
                 params=params+'&'+$('#'+_options.formId).serialize();
-                // params=$.param(_options.queryParams)+'&'+$('#'+_options.formId).serialize();
             }
             if(_options.queryParams){
                 params=params+'&'+$.param(_options.queryParams);
             }
-           
             $.post(_options.url,params,
                 function(data){
-                    tmpBoDataGridData.data=data;
-                    defaultDataGridParams.total = data.total;
-                    $('#pageContent').html('当前显示第'+_options.page+'页/每页显示'+_options.rows+'条数据/共'+defaultDataGridParams.total+'条数据');
+                    tmpBoDataGridData[_this[0].id]=data;
+                    defaultDataGridParams[_this[0].id].total = data.total;
+                    $('#pageContent-'+_options.namespace).html('当前显示第'+_options.page+'页/每页显示'+_options.rows+'条数据/共'+defaultDataGridParams[_this[0].id].total+'条数据');
                     for(var i=0;i<data.rows.length;i++){
                         $tr=$('<tr index="index-'+i+'"></tr>');
+                        if(_options.checkbox){
+                           var $dataCheckBox=$('<td><input id="dataCheckBox-'+_options.namespace+'-'+i+'" type="checkbox"/></td>');
+                           $tr.append($dataCheckBox);
+                        }
                         var thWidth='100px';
                         for(var j=0;j<_options.columns[0].length;j++){
                             if(_options.columns[0][j].width&&_options.columns[0][j].width!=''){
@@ -245,30 +263,40 @@
                             }
                             if(_options.columns[0][j].formatter&&typeof _options.columns[0][j].formatter == "function"){
                                 var _val=_options.columns[0][j].formatter(data.rows[i][_options.columns[0][j].field],data.rows[i])
-                                $tr.append('<td><div style="width:'+thWidth+';overflow:hidden">'+_val+'</div></td>');
+                                var $td=$('<td><div style="width:'+thWidth+';overflow:hidden">'+_val+'</div></td>');
+                                if(_options.columns[0][j].hidden){
+                                    $td.hide();
+                                }
+                                $tr.append($td);
                             }else{
-                                $tr.append('<td title="'+data.rows[i][_options.columns[0][j].field]+'"><div style="width:'+thWidth+';overflow:hidden">'+data.rows[i][_options.columns[0][j].field]+'</div></td>');
+                                var $td=$('<td title="'+data.rows[i][_options.columns[0][j].field]+'"><div style="width:'+thWidth+';overflow:hidden">'+data.rows[i][_options.columns[0][j].field]+'</div></td>');
+                                if(_options.columns[0][j].hidden){
+                                    $td.hide();
+                                }
+                                $tr.append($td);
                             }
                         }
                         $dataDiv.append($tr);
                     }
                     $tableData.append($dataDiv);
-                    $('#boDataGridLoading-'+namespace).modal('hide');
+                    $('#boDataGridLoading-'+_options.namespace).modal('hide');
                     if(typeof _options.onLoadSuccess== "function"){
                         _options.onLoadSuccess(data);
                     }
                     
-                    $('#tableData-'+namespace+' tr').click(function(){
+                    $('#tableData-'+_options.namespace+' tr').click(function(){
                         var _index=$(this).attr('index').slice(6);
-                        var _row=tmpBoDataGridData.data.rows[_index];
+                        var _row=tmpBoDataGridData[_this[0].id].rows[_index];
+                        console.info(_row);
                         if(typeof _options.onClickRow == "function"){
                             _options.onClickRow(_index,_row);
                         }
                     });
 
-                    $('#tableData-'+namespace+' tr').dblclick(function(){
+                    $('#tableData-'+_options.namespace+' tr').dblclick(function(){
                         var _index=$(this).attr('index').slice(6);
-                        var _row=tmpBoDataGridData.data.rows[_index];
+                        var _row=tmpBoDataGridData[_this[0].id].rows[_index];
+                        console.info(_row);
                         if(typeof _options.onDblClickRow == "function"){
                             _options.onDblClickRow(_index,_row);
                         }
@@ -276,10 +304,10 @@
             }, "json");     
         },
         loading:function(){
-          $('#boDataGridLoading-'+namespace).modal('show'); 
+          $('#boDataGridLoading-'+_options.namespace).modal('show'); 
         },
         loaded:function(){
-          $('#boDataGridLoading-'+namespace).modal('hide');
+          $('#boDataGridLoading-'+_options.namespace).modal('hide');
         },
         getRows:function(){
           return tmpBoDataGridData.data.rows;
